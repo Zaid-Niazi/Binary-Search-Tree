8ddf8dabdd2699a2cb3548e49a81dfca7e6ec425
vazcon <zaidniaazi@gmail.com>
2024-11-20 08:39:55 +0530

    Add Tree Class & Sorter Function

--- index.js
+++ index.js
@@ -5,3 +5,30 @@ class Node {
     this.right = right;
   }
 }
+
+class Tree {
+  buildTree(array) {
+    let arr = sorter(array);
+    return arr;
+  }
+
+  root = this.buildTree(array);
+}
+
+function sorter(array) {
+  array.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1]) {
+      array.splice(array[i], 1);
+    }
+  }
+  return array;
+}
+
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const test = new Tree();
+console.log(test.buildTree(array));
